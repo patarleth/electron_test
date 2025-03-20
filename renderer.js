f2086563d9f345bebf83bf366978d6c94ae6961e
Pat Arleth <patarleth@gmail.com>
2025-03-19 20:07:39 -0400

    retain existing button onclick behavior in addition to node ipc call

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@ const buttonIds = [
     'doh_button', 'default_button', 'danger_button', 'dropdown_button',
     'dropdown_active', 'dropdown_1', 'dropdown_item2', 'dropdown_item3',
     'dropdown_item4', 'hover_click_button', 'model_open_button',
-    'model_cancel_button','model_save_button'
+    'model_cancel_button', 'model_save_button'
 ]
 
 window.onload = function(e) {
@@ -13,12 +13,17 @@ window.onload = function(e) {
     buttonIds.forEach((id) => {
         const next = document.getElementById(id);
         if(next) { 
+            const prevOnClick = next.onclick
             next.onclick = () => { 
                 ipcRenderer.invoke(id, 'clicked').then( (result) => {
                     const msg = 'button ' + id + ' returned ' + result
                     console.log(msg)
                     alertBox.innerHTML = msg
                 })
+                if (prevOnClick) {
+                    console.log(id + ' has existing onclick')
+                    prevOnClick()
+                }
             }
         } else {
             console.log(id +' not found')
